dfb908391f80af46cc65526b2552d7422382c62a
dyoun12 <gdaeyeon002@gmail.com>
2025-05-07 00:49:05 +0900

    feat: vuetify 패키지 적용

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,6 +4,18 @@ import '@/style.css'
 import App from '@/App.vue'
 import { router } from '@/router'
 
+// Vuetify 관련 import
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 const pinia = createPinia()
 
