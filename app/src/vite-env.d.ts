db23b8064bc4cff1bcf1fb010355c01f96fe8a65
dyoun12 <gdaeyeon002@gmail.com>
2025-05-06 20:13:37 +0900

    refactor: import 간편화를 위한 path alias 추가 - @/ -> src/

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// ts에서 인식할 .vue 파일 모듈 정의
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
