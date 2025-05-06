ea305b85b4ea9feea7d530941a094d60452e1551
dyoun12 <gdaeyeon002@gmail.com>
2025-05-06 20:14:59 +0900

    feat: 기본 폴더 구조 적용

---
+++ app/src/components/layout/Sidebar.vue
@@ -0,0 +1,5 @@
+<template>
+  <nav>
+    <router-link to="/">Dashboard</router-link>
+  </nav>
+</template>
---
+++ app/src/frames/MainFrame.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Sidebar from '@/components/layout/Sidebar.vue'
+</script>
+
+<template>
+  <div class="main-frame">
+    <Sidebar />
+    <div class="content">
+      <h1>hiu</h1>
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-frame {
+  display: flex;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
+</style>
