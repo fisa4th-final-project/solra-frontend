327f29c6b91c920b211ccaddcdeaee0efc9137cd
dyoun12 <gdaeyeon002@gmail.com>
2025-05-09 16:48:58 +0900

    fix: globalErrorHandler import문 제거

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 import '@/style.css'
 import App from '@/App.vue'
 import { router } from '@/router'
-import { globalErrorHandler } from '@/lib/global/errorHandler'
 
 // Vuetify 관련 import
 import '@mdi/font/css/materialdesignicons.css'
@@ -34,8 +33,6 @@ const vuetify = createVuetify({
 
 const app = createApp(App)
 const pinia = createPinia()
-
-// app.config.errorHandler = globalErrorHandler
 
 app.use(pinia)
 app.use(router)
