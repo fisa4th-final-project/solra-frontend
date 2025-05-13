d5110b7e633633dc6da1f9e1bab6751b0162d83b
dyoun12 <gdaeyeon002@gmail.com>
2025-05-13 10:47:15 +0900

    feat: logout로직 추가

---
+++ app/src/lib/api/user/logoutApi.ts
@@ -0,0 +1,30 @@
+import { apiRequest } from "@/lib/global/apiHandler";
+import { ApiError } from "@/lib/global/customError";
+import { router } from "@/router";
+import { useDialogStore } from "@/store/dialog";
+import { useAuthStore } from "@/store/auth";
+import type { ApiResponse } from "@/lib/global/ApiResponse";
+
+export async function logoutApi() {
+
+  const dialog = useDialogStore();
+  const auth = useAuthStore();
+
+  await apiRequest({
+    method: "POST",
+    path: "api/auth/logout"
+  }).then(async (res: ApiResponse<null>) => {
+    if (res.data) {
+      auth.logout();
+      console.log(auth.$state);
+      await router.push('/login');
+    }
+  }).catch((e: ApiError) => {
+    console.error(e.res);
+    dialog.open({
+      title: '로그아웃 실패',
+      message: e.res.message
+    });
+
+  });
+}
