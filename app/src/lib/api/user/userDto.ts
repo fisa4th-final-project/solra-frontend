2a78155a38c244343cedd3793e2a0a756a2caefd
dyoun12 <gdaeyeon002@gmail.com>
2025-05-15 12:25:45 +0900

    feat: user 관련 api Dto 추가

--- app/src/lib/api/user/userDto.ts
+++ app/src/lib/api/user/userDto.ts
@@ -27,3 +27,98 @@ export interface WhomiResponseDto {
   departmentId?: number,
   organizationId?: number
 }
+
+export interface CreateUserRequestDto {
+  userLoginId: string,
+  password: string,
+  userName: string,
+  email: string,
+  orgId?: number,
+  deptId?: number
+}
+
+export interface CreateUserResponseDto {
+  userId: number,
+  userName: string,
+  userLoginId: string,
+  email: string,
+  departmentId?: number,
+  organizationId?: number
+}
+
+export interface UpdateUserRequestDto {
+  userId: number
+  userLoginId: string,
+  userName: string,
+  password: string,
+  email: string,
+}
+
+export interface UpdateUserResponseDto {
+  userId: number,
+  userName: string,
+  userLoginId: string,
+  email: string,
+  departmentId?: number,
+  organizationId?: number
+}
+
+export interface GetUsersQueryParam {
+  page: number,
+  size: number
+}
+
+export interface GetUsersResponseDto {
+  content: GetUsersResContent[],
+  pageable: {
+    pageNumber: number,
+    pageSize: number,
+    sort: {
+      empty: boolean,
+      sorted: boolean,
+      unsorted: boolean
+    },
+    offset: number,
+    paged: boolean,
+    unpaged: boolean
+  },
+  last: boolean,
+  totalPages: number,
+  totalElements: number,
+  size: number,
+  number: number,
+  sort: {
+    empty: boolean,
+    sorted: boolean,
+    unsorted: boolean
+  },
+  first: boolean,
+  numberOfElements: number,
+  empty: boolean
+}
+
+export interface GetUsersResContent {
+  userId: number,
+  userName: string,
+  userLoginId: string,
+  email: string,
+  departmentId?: number,
+  organizationId?: number
+}
+
+export interface GetUserDetailQueryParam {
+  userId: number
+}
+
+export interface GetUserDetailResponseDto {
+  userId: number,
+  userName: string,
+  userLoginId: string,
+  email: string,
+  departmentId?: number,
+  organizationId?: number
+}
+
+export interface DeleteUserQueryParam {
+  userId: number
+}
