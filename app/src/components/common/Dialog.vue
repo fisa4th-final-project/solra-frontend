6e5005385e45192733184910fb93f5956957aa48
dyoun12 <gdaeyeon002@gmail.com>
2025-05-15 12:11:31 +0900

    refactor: dialog 슬롯 및 타입 구분 추가

--- app/src/components/common/Dialog.vue
+++ app/src/components/common/Dialog.vue
@@ -1,18 +1,43 @@
-<script setup lang="ts">
-import { useDialogStore } from '@/store/dialog';
-const dialogStore = useDialogStore();
-</script>
-
 <template>
-  <v-dialog v-model="dialogStore.showDialog" max-width="400">
-    <v-card>
-      <v-card-title class="text-h5">{{ dialogStore.dialogTitle }}</v-card-title>
-      <v-card-text>{{ dialogStore.dialogMessage }}</v-card-text>
+  <v-dialog 
+    v-model="dialogStore.showDialog" 
+    min-width="400"
+    theme="light"
+    
+  >
+    <v-card
+      rounded="lg"
+      flat
+      style="
+        padding: 20px 0;
+        border: 2px solid #DFE0EB;
+        "
+    >
+      <v-card-title class="text-h5">
+        <slot name="title">
+          {{ dialogStore.dialogTitle }}
+        </slot>
+      </v-card-title>
+
+      <v-card-text>
+        <slot name="default">
+          {{ dialogStore.dialogMessage }}
+        </slot>
+      </v-card-text>
+
       <v-card-actions>
         <v-spacer />
-        <v-btn text @click="dialogStore.close">확인</v-btn>
+        <slot name="actions">
+          <v-btn text @click="dialogStore.close">확인</v-btn>
+        </slot>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
+<script setup lang="ts">
+import { useDialogStore } from '@/store/dialog';
+const dialogStore = useDialogStore();
+
+
+</script>
