c178ce737b7002a98bfca908e7f73855a21891dc
dyoun12 <gdaeyeon002@gmail.com>
2025-05-15 12:16:32 +0900

    feat: page 공용 header 추가

---
+++ app/src/components/layout/PageHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-breadcrumbs 
+    :items="items"
+    style="
+      font-size: large;
+      padding: 0;
+      padding-bottom: 30px;
+    "
+    :color="color"
+  >
+  <template v-slot:divider>
+    <v-breadcrumbs-divider
+    >
+    <p :style="{color:color}">
+      /
+    </p>
+    </v-breadcrumbs-divider>
+  </template>
+  </v-breadcrumbs>
+</template>
+
+<script lang="ts" setup>
+
+  export interface Item {
+    title: string,
+    disabled: boolean,
+    href: string
+  }
+
+  defineProps<{
+    items: Item[],
+    color: string,
+  }>();
+
+</script>
