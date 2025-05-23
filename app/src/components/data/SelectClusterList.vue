40d8d7c1d3991b2640477c23b3481c29a15f8d92
dyoun12 <gdaeyeon002@gmail.com>
2025-05-24 00:07:07 +0900

    feat: SelectComponents 추가 - SelectClusterList - SelectNsList

---
+++ app/src/components/data/SelectClusterList.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-select
+    v-model="form.cluster"
+    :items="clusterList"
+    :rules="[rules.required]"
+    variant="underlined"
+    label="클러스터 선택"
+    color="primary"
+    item-title="name"
+    item-value="clusterId"
+    return-object
+  />
+</template>
+<script lang="ts" setup>
+
+import { onMounted, ref } from 'vue';
+import { getClustersApi } from '@/lib/api/cluster/getClustersApi';
+import { rules } from '@/lib/global/inputRules';
+
+defineProps<{
+  form: {
+    cluster: {
+      clusterId: number,
+      name: string
+    }
+  }
+}>();
+
+const clusterList = ref<{
+  clusterId: number;
+  name: string;
+}[]>();
+
+onMounted(async () => {
+  await getClustersApi().then((res) => {
+    if (res) clusterList.value = res;
+  });
+});
+
+</script>
