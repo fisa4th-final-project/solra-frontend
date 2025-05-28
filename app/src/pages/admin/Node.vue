15f74bcdc33d5957811651aa9dc40537c226542b
dyoun12 <gdaeyeon002@gmail.com>
2025-05-28 16:02:09 +0900

    feat: info 버튼 - 클러스터 상세 조회 기능 추가

--- app/src/pages/admin/Node.vue
+++ app/src/pages/admin/Node.vue
@@ -1,27 +1,35 @@
 <template>
-  <v-container v-if="clusterList.length"> <!-- TODO: 클러스터 별 Node 리스트 출력 -->
-    <v-row  v-for="(cluster, i) in clusterList" :key="i">
+  <v-container height="100%">
+    <v-row align="center">
+      <v-col>
+        <span class="text-h4">
+          노드
+        </span>
+      </v-col>
+    </v-row>
+    <v-row v-if="clusterList.length" v-for="(cluster, i) in clusterList" :key="i">
       <v-col>
         <v-row justify="space-between" align="center" class="px-3">
-            <span class="text-h5 text-uppercase pa-0">
-              {{ cluster.name }}
-            </span>
-            <v-col>
-              <v-row v-if="isEmptyList[cluster.clusterId]">
-                <v-col>
-                  <v-progress-linear 
-                    height="1"
-                    indeterminate
-                  />
-                </v-col>
-              </v-row>
-              <v-divider/>
-            </v-col>
-            
-            <v-icon> <!-- TODO: cluster 정보 조회 기능 추가 -->
-              mdi-information-outline
-            </v-icon>   
-          </v-row>
+          <span class="text-h5 text-uppercase pa-0">
+            {{ cluster.name }}
+          </span>
+          <v-col>
+            <v-row v-if="isEmptyList[cluster.clusterId]">
+              <v-col>
+                <v-progress-linear 
+                  height="1"
+                  indeterminate
+                />
+              </v-col>
+            </v-row>
+            <v-divider/>
+          </v-col>
+          
+          <v-icon @click="openClusterDetail(cluster)">
+            mdi-information-outline
+          </v-icon>
+          
+        </v-row>
         <GetNodeList 
           :req="{clusterId: cluster.clusterId}"
           @selected="select"
@@ -30,15 +38,43 @@
         <v-spacer v-if="i + 1 < clusterList.length" class="ma-15" />
       </v-col>
     </v-row>
+    <v-container v-else  height="100%" class="d-flex justify-center align-center">
+      <v-progress-circular
+      :width="3"
+      size="50"
+      color="primary"
+      indeterminate
+      />
+    </v-container>
   </v-container>
-  <v-container v-else  height="100%" class="d-flex justify-center align-center">
-    <v-progress-circular
-    :width="3"
-    size="50"
-    color="primary"
-    indeterminate
-    />
-  </v-container>
+  <SideContents v-model="isClusterDetailOpen">
+    <template v-slot:title>
+      <v-row justify="space-between" align="center">
+        <v-col>
+          클러스터 상세
+        </v-col>
+        <v-col align="end">
+          <UpdateCluster
+            v-if="selectedCluster"
+            :clusterId="selectedCluster.clusterId"
+          >
+            <template v-slot:activator="{props}">
+              {{ console.log(selectedCluster.clusterId) }}
+              {{ console.log(props) }}
+              <v-btn 
+                v-bind="props"
+                color="primary"
+                variant="plain"
+              >
+                Edit
+              </v-btn>
+            </template>
+          </UpdateCluster>
+        </v-col>
+      </v-row>
+    </template>
+    <GetClusterDetail  v-if="selectedCluster" :title="selectedCluster.name" :req="{clusterId: selectedCluster.clusterId}" />
+  </SideContents>
 </template>
 <script lang="ts" setup>
 
@@ -47,17 +83,29 @@ import GetNodeList from '@/components/data/GetNodeList.vue';
 import { getClustersApi } from '@/lib/api/cluster/getClustersApi';
 import type { GetClustersResponseDto } from '@/lib/api/cluster/clusterDto';
 import type { GetNodeDetailResponseDto } from '@/lib/api/node/nodeDto';
+import SideContents from '@/components/layout/SideContents.vue';
+import GetClusterDetail from '@/components/data/GetClusterDetail.vue';
+import UpdateCluster from '@/components/data/UpdateCluster.vue';
 
 const clusterList = ref<GetClustersResponseDto[]>([]);
 const selectedNode = ref<GetNodeDetailResponseDto>();
 const isEmptyList = ref<Record<number, boolean>>({});
 
+const selectedCluster = ref();
+
+const isClusterDetailOpen = ref(false);
 
 const select = (item: GetNodeDetailResponseDto) => {
   selectedNode.value = item;
 }
 const isEmpty = (clusterId: number, empty: boolean) => {
   isEmptyList.value[clusterId] = empty;
+}
+
+const openClusterDetail = (cluster: any) => {
+  selectedCluster.value = cluster;
+  console.log(selectedCluster.value);
+  isClusterDetailOpen.value = true;
 }
 
 onMounted(async() => {
