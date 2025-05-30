c2b494ffe2438e476260d6fa9bf1152538fc557c
dyoun12 <gdaeyeon002@gmail.com>
2025-05-30 20:44:21 +0900

    feat: "@test" alias 추가 및 setup.ts 파일 생성

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -33,7 +33,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      '@test': path.resolve(__dirname, './test')
     }
   }
 })
