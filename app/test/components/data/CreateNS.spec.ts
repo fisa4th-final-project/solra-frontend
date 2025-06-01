5da56108abb8d356e6452cc2c7234ecf675301e8
dyoun12 <gdaeyeon002@gmail.com>
2025-06-01 15:37:15 +0900

    refactor: unUsed import 제거

--- app/test/components/data/CreateNS.spec.ts
+++ app/test/components/data/CreateNS.spec.ts
@@ -1,5 +1,5 @@
 import { flushPromises, mount } from '@vue/test-utils'
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { nextTick } from 'vue';
 import SideContents from '@/__mocks__/layout/SideContents.vue';
 import CreateNS from '@/components/data/CreateNS.vue';
