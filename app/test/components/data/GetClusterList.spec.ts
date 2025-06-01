21c36f24f95f0641f1be01474bacd06521d3944f
dyoun12 <gdaeyeon002@gmail.com>
2025-06-01 17:06:34 +0900

    test: GetClusterList 컴포넌트 테스트 추가

---
+++ app/test/components/data/GetClusterList.spec.ts
@@ -0,0 +1,92 @@
+import { mount, flushPromises } from '@vue/test-utils'
+import { describe, it, vi, expect } from 'vitest'
+import GetClusterList from '@/components/data/GetClusterList.vue'
+import * as apiHandler from '@/lib/api/cluster/getClusterListApi';
+
+const getClusterListWrapper = () => {
+  return mount(GetClusterList, {
+    props: {
+      req: {
+        orgId: 2
+      }
+    }
+  });
+};
+
+describe('GetClusterList.vue', () => {
+
+  const mockResponse = [
+    {
+      clusterId: 1,
+      orgId: 2,
+      name: 'test-cluster1',
+      env: 'dev',
+      caCert: 'hidden',
+      saToken: 'hidden',
+      apiServerUrl: 'https://...',
+      createdAt: new Date('2024-01-01'),
+      updatedAt: new Date('2024-01-02')
+    },
+    {
+      clusterId: 2,
+      orgId: 2,
+      name: 'test-cluster2',
+      env: 'dev',
+      caCert: 'hidden',
+      saToken: 'hidden',
+      apiServerUrl: 'https://...',
+      createdAt: new Date('2024-01-01'),
+      updatedAt: new Date('2024-01-02')
+    }
+  ];
+
+  it('TC_VUE_CLUSTER_04_01: skeleton 표시 후 label 정상 렌더링', async () => {
+    // skeleton을 테스트하기 위한 mock 구현
+    vi.spyOn(apiHandler, 'getClusterListApi').mockImplementation(() => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(mockResponse);
+        }, 10); // 10ms 지연
+      });
+    });
+
+    const wrapper = getClusterListWrapper();
+
+    const skeletonLabel = '정보를 불러올 수 없습니다.'
+    // skeleton 표시 확인 (응답 오기 전)
+    expect(wrapper.html()).toContain(skeletonLabel);
+
+    // API 응답 처리 완료 대기
+    await flushPromises();
+
+    // skeleton 사라지고 label 확인
+    const labels = Object.keys(mockResponse);
+    labels.forEach(label => {
+      expect(wrapper.html()).toContain(label);
+    });
+
+    wrapper.unmount();
+  });
+  it('TC_VUE_CLUSTER_04_02: 빈 List 응답 시 skeleton 렌더링 여부', async () => {
+    // skeleton을 테스트하기 위한 mock 구현
+    vi.spyOn(apiHandler, 'getClusterListApi').mockImplementation(() => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve([]);
+        }, 10); // 10ms 지연
+      });
+    });
+
+    const wrapper = getClusterListWrapper();
+    
+    // API 응답 처리 완료 대기
+    await flushPromises();
+    
+    // skeleton 표시 확인 (빈 List 응답 시)
+    const skeletonLabel = '정보를 불러올 수 없습니다.'
+    expect(wrapper.html()).toContain(skeletonLabel);
+
+    wrapper.unmount();
+
+  });
+});
