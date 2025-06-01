7c44de36747ed6e7f97880065905612bf16e4ead
dyoun12 <gdaeyeon002@gmail.com>
2025-06-01 15:04:57 +0900

    test: DeleteOrg 컴포넌트 테스트 추가

---
+++ app/test/components/data/DeleteOrg.spec.ts
@@ -0,0 +1,41 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import { nextTick } from 'vue';
+import DeleteOrg from '@/components/data/DeleteOrg.vue';
+
+describe('DeleteOrg.vue', () => {
+
+  it('TC_VUE_ORG_02_01: 클릭 후 삭제 Dialog 렌더링 성공', async () => {
+
+    const el = document.createElement('div')
+    document.body.appendChild(el)
+
+    const wrapper = mount(DeleteOrg, {
+      attachTo: el, // Teleport가 실제 DOM에 붙도록 설정
+      props: {
+        org: {
+          orgId: 1,
+          orgName: 'test-orgName'
+        }
+      }
+    });
+
+    await wrapper.find('button').trigger('click');
+    await nextTick();
+    
+    const labels = [
+      '조직을 삭제하시겠습니까?',
+      '조직: test-orgName',
+      '취소',
+      '삭제'
+    ]
+
+    labels.forEach(label => {
+      expect(document.body.innerHTML).toContain(label);
+    });
+
+    wrapper.unmount();
+    document.body.removeChild(el);
+  });
+
+});
