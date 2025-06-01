fe265d16394b3d6ec9a9d4fa16e8292976e030ec
dyoun12 <gdaeyeon002@gmail.com>
2025-06-01 19:40:28 +0900

    test: GetPodDetail 컴포넌트 테스트 추가

---
+++ app/test/components/data/GetPodDetail.spec.ts
@@ -0,0 +1,54 @@
+import { mount, flushPromises } from '@vue/test-utils'
+import { describe, it, vi, expect } from 'vitest'
+import GetPodDetail from '@/components/data/GetPodDetail.vue'
+import * as apiHandler from '@/lib/api/pod/getPodDetail.Api';
+
+const getPodDetailWrapper = () => {
+  return mount(GetPodDetail, {
+    props: {
+      title: 'test-pod-detail-title',
+      req: {
+        clusterId: 1,
+        nsName: 'test-nsName',
+        name: 'test-podName'
+      }
+    }
+  });
+};
+
+describe('GetPodDetail.vue', () => {
+
+  const mockResponse = {
+    name: 'test-pod-name',
+    phase: 'test-pod-phase',
+    podIP: 'test-pod-ip',
+    nodeName: 'test-nodeName'
+  };
+
+  it('TC_VUE_POD_03_01: skeleton 표시 후 label 정상 렌더링', async () => {
+    // skeleton을 테스트하기 위한 mock 구현
+    vi.spyOn(apiHandler, 'getPodDetailApi').mockImplementation(() => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(mockResponse);
+        }, 10); // 10ms 지연
+      });
+    });
+
+    const wrapper = getPodDetailWrapper();
+
+    // skeleton 표시 확인 (응답 오기 전)
+    expect(wrapper.findComponent({ name: 'VProgressCircular' }).exists()).toBe(true);
+
+    // API 응답 처리 완료 대기
+    await flushPromises();
+
+    // skeleton 사라지고 label 확인
+    const labels = Object.keys(mockResponse);
+    labels.forEach(label => {
+      expect(wrapper.html()).toContain(label);
+    });
+
+    wrapper.unmount();
+  });
+});
