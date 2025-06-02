9845a5e35e61861318ca896ee78d23f09a1c0966
dyoun12 <gdaeyeon002@gmail.com>
2025-06-02 11:22:51 +0900

    test: UpdateUser 컴포넌틑 테스트 추가

---
+++ app/test/components/data/UpdateUser.spec.ts
@@ -0,0 +1,43 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import { nextTick } from 'vue';
+import UpdateUser from '@/components/data/UpdateUser.vue';
+import Content from '@/__mocks__/common/Content.vue';
+
+describe('UpdateUser.vue', () => {
+
+  it('TC_VUE_USER_06_01: 클릭 후 입력 form 렌더링 성공', async () => {
+
+    const el = document.createElement('div')
+    document.body.appendChild(el)
+
+    const wrapper = mount(UpdateUser, {
+      attachTo: el, // Teleport가 실제 DOM에 붙도록 설정
+      global: {
+        stubs: {
+          DeleteUser: Content,
+        }
+      },
+      props: {
+        userId: 1
+      }
+    });
+
+    await wrapper.find('button').trigger('click');
+    await nextTick();
+    
+    const labels = [
+      '사용자 ID',
+      '비밀번호',
+      '이름',
+      '이메일',
+    ]
+
+    labels.forEach(label => {
+      expect(document.body.innerHTML).toContain(label);
+    });
+
+    wrapper.unmount();
+    document.body.removeChild(el);
+  });
+});
