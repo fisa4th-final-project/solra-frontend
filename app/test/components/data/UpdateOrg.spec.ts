4cab240e4132069daa72e5de4edef56b817cc47e
dyoun12 <gdaeyeon002@gmail.com>
2025-06-02 10:51:18 +0900

    test: UpdateOrg 컴포넌트 테스트 추가

---
+++ app/test/components/data/UpdateOrg.spec.ts
@@ -0,0 +1,41 @@
+import { flushPromises, mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import { nextTick } from 'vue';
+import SideContents from '@/__mocks__/layout/SideContents.vue';
+import UpdateOrg from '@/components/data/UpdateOrg.vue';
+
+const updateOrgWrapper = () => {
+  return mount(UpdateOrg, {
+    global: {
+      stubs: {
+        SideContents: SideContents,
+      }
+    }
+  });
+}
+describe('UpdateOrg.vue', () => {
+
+  it('TC_VUE_ORG_05_01: 클릭 후 입력 form 렌더링 성공', async () => {
+
+    const el = document.createElement('div')
+    document.body.appendChild(el)
+
+    const wrapper = mount(UpdateOrg, {
+      attachTo: el // Teleport가 실제 DOM에 붙도록 설정
+    });
+
+    await wrapper.find('button').trigger('click');
+    await nextTick();
+    
+    const labels = [
+      '수정할 조직 이름'
+    ]
+
+    labels.forEach(label => {
+      expect(document.body.innerHTML).toContain(label);
+    });
+
+    wrapper.unmount();
+    document.body.removeChild(el);
+  });
+});
