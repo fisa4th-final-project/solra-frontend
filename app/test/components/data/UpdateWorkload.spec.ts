4c3e1063971ac42efcaf6e555e5fe11caa0f8ca3
dyoun12 <gdaeyeon002@gmail.com>
2025-06-02 11:26:00 +0900

    test: UpdateWorkload 컴포넌트 테스트 추가

---
+++ app/test/components/data/UpdateWorkload.spec.ts
@@ -0,0 +1,47 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import { nextTick } from 'vue';
+import UpdateWorkload from '@/components/data/UpdateWorkload.vue';
+import Content from '@/__mocks__/common/Content.vue';
+
+describe('UpdateWorkload.vue', () => {
+
+  it('TC_VUE_WORKLOAD_05_01: 클릭 후 입력 form 렌더링 성공', async () => {
+
+    const el = document.createElement('div')
+    document.body.appendChild(el)
+
+    const wrapper = mount(UpdateWorkload, {
+      attachTo: el, // Teleport가 실제 DOM에 붙도록 설정
+      global: {
+        stubs: {
+          UpdateSvcInWorkload: Content,
+          UpdateDeployInWorkload: Content
+        }
+      },
+      props: {
+        cluster: {
+          clusterId: 1,
+        },
+        ns: {
+          name: 'test-nsName'
+        }
+      }
+    });
+
+    await wrapper.find('button').trigger('click');
+    await nextTick();
+    
+    const labels = [
+      '워크로드 수정'
+    ]
+
+    labels.forEach(label => {
+      expect(document.body.innerHTML).toContain(label);
+    });
+
+    wrapper.unmount();
+    document.body.removeChild(el);
+  });
+
+});
