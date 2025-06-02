42a060c2bdf811ab72b78fdba1e1e6bdff3eab23
dyoun12 <gdaeyeon002@gmail.com>
2025-06-02 10:48:38 +0900

    test: UpdateNS 컴포넌트 테스트 추가

---
+++ app/test/components/data/UpdateNS.spec.ts
@@ -0,0 +1,36 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import { nextTick } from 'vue';
+import UpdateNS from '@/components/data/UpdateNS.vue';
+
+describe('UpdateNS.vue', () => {
+
+  it('TC_VUE_NAMESPACE_05_01: 클릭 후 입력 form 렌더링 성공', async () => {
+
+    const el = document.createElement('div')
+    document.body.appendChild(el)
+
+    const wrapper = mount(UpdateNS, {
+      attachTo: el, // Teleport가 실제 DOM에 붙도록 설정
+      props: {
+        clusterId: 1,
+        name: 'test-nsName'
+      }
+    });
+
+    await wrapper.find('button').trigger('click');
+    await nextTick();
+    
+    const labels = [
+      '네임스페이스 labels',
+      '네임스페이스 annotaions'
+    ]
+
+    labels.forEach(label => {
+      expect(document.body.innerHTML).toContain(label);
+    });
+
+    wrapper.unmount();
+    document.body.removeChild(el);
+  });
+});
