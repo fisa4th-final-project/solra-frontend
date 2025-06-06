30a79248900eb80399f24d26d9eb67d3c6688306
dyoun12 <gdaeyeon002@gmail.com>
2025-06-06 17:53:51 +0900

    feat: 로그인 상태, 권한 별 접근 검증에 대한 router 가드 설정

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -6,4 +6,37 @@ import { apiHandler } from '@/lib/global/apiManager';
 export const router = createRouter({
   history: createWebHistory(),
   routes
-})
+});
+
+// 로그인 세션 검증
+router.beforeEach(async (to, _from, next) => {
+  const auth = useAuthStore();
+
+  // isLogined false 이면 whomi() 요청으로 백엔드에서 한번 더 검증
+  if (to.name !== 'Login' && !auth.getIsLogined) await apiHandler.whomiApi();
+
+  // 이후에도 isLogined false이면 로그인 화면으로 라우팅
+  if (to.name !== 'Login' && !auth.getIsLogined) {
+    next({ name: 'Login' });
+  } else {
+    next();
+  }
+});
+
+// 역할 검증
+router.beforeEach((to, _from, next) => {
+  const auth = useAuthStore(); 
+  const requireRoles = to.meta.role;
+
+  const userRoles = auth.getRoles.map((role) => role.roleName);
+
+  if (!requireRoles) return next();
+
+  const hasAccess = requireRoles?.some(role => userRoles.includes(role));
+
+  if (hasAccess) {
+    next()
+  } else {
+    alert('해당 페이지에 대한 접근 권한이 없습니다.');
+  }
+});
--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="vite/client" />
 
+// route.meta.role 타입 추가
+import 'vue-router'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    role?: string[]
+  }
+}
+
 // ts에서 인식할 .vue 파일 모듈 정의
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
@@ -9,3 +18,4 @@ declare module '*.vue' {
 
 // vuetify/styles 모듈 추가
 declare module 'vuetify/styles';
+
