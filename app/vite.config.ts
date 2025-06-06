86fe5f9793064530a84418f8fc01eed2611cffa5
dyoun12 <gdaeyeon002@gmail.com>
2025-06-06 15:05:17 +0900

    feat: local 환경 https 구성

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import vuetify from 'vite-plugin-vuetify'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,5 +19,14 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-cert.pem')),
+    },
+    port: 5173,
+    host: 'localhost',
+    cors: true
   }
 })
