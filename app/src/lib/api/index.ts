03d5f2e068ca4d3aa5c4f0cbffa85ab1c9836fa3
dyoun12 <gdaeyeon002@gmail.com>
2025-06-09 02:06:35 +0900

    feat: deleteUserRole api 핸들러 및 dto 추가

--- app/src/lib/api/index.ts
+++ app/src/lib/api/index.ts
@@ -80,3 +80,4 @@ export { whomiApi } from "@/lib/api/user/whomiApi";
 // UserRole
 export { getUserRoleByUserIdApi } from "@/lib/api/userRole/getUserRoleApi";
 export { createUserRoleApi } from "@/lib/api/userRole/createUserRoleApi";
+export { deleteUserRoleApi } from "@/lib/api/userRole/deleteUserRoleApi";
