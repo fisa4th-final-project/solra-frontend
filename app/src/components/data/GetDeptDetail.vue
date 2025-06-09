6a482ce4b56978a5badb01785b7218da3451a8a6
dyoun12 <gdaeyeon002@gmail.com>
2025-06-09 11:58:18 +0900

    fix: dept_read 권한 부재 시 자신이 속한 부서 상세 출력

--- app/src/components/data/GetDeptDetail.vue
+++ app/src/components/data/GetDeptDetail.vue
@@ -21,6 +21,33 @@
       </v-container>
     </template>
   </DataCard>
+  <DataCard
+    :header="{title, icon: 'mdi-briefcase'}"
+    :data="{
+      orgId: auth.getMe.auth.orgId, 
+      orgName: auth.getMe.user.orgName,
+      deptId: auth.getMe.auth.deptId, 
+      deptName: auth.getMe.user.deptName
+    }"
+    v-else
+  >      
+    <template v-slot:item="{ item }">
+      <tr v-if="item.field !== 'orgId' && item.field !== 'deptId'">
+        <th>{{ item.field }}</th>
+        <td class="text-right">{{ item.value }}</td>
+      </tr>
+    </template>
+    <template v-slot:skeleton>
+      <v-container height="150px" class="d-flex justify-center align-center">
+        <v-progress-circular
+          indeterminate
+          color="primary"
+          size="60"
+          width="6"
+        />
+      </v-container>
+    </template>
+  </DataCard>
 </template>
 <script lang="ts" setup>
 
